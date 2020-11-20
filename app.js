8012702b2bf864f5cc61a9f78f98d62338dda6ca
David Angeles Ungson <davidangelesu@gmail.com>
2020-11-20 17:51:11 +0100

    added cors

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const io = require("socket.io")(server);
+const cors = require("cors");
 
 const port = process.env.PORT || 4000;
+app.use(cors());
 //For Showing if Server is Up
 app.get("/", (req, res) => {
 	res.send("<h1>Socket Server up and Running...</h1>");
@@ -23,12 +25,12 @@ io.on("connection", (socket) => {
 
 	//Listener to update Location of Entity
 	//TODO ADD Autentication....
-	socket.on("update-location", (projectId,entityId,location) => {    
-        //emits entity-new-location event to room (projectId-entityId);
-        const room = projectId.toLowerCase().concat(entityId.toLowerCase());
-        console.log("updating location of ");
-        console.log(entityId)
-		socket.broadcast.to(room).emit("entity-new-location", location);
+	socket.on("update-location", (projectId, entityId, location) => {
+		//emits entity-new-location event to room (projectId-entityId);
+		const room = projectId.toLowerCase().concat(entityId.toLowerCase());
+		console.log("updating location of ");
+		console.log(entityId);
+		socket.broadcast.to(room).emit("entity-new-location", entityId, location);
 	});
 
 	//Listener for Leaving room
