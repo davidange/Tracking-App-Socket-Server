0f45e933c4814f86c486c8e1512067ac0d14936a
David Angeles Ungson <davidangelesu@gmail.com>
2020-11-20 15:49:49 +0100

    Initial commit of Tracking API Socket Server

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+
+const port = process.env.PORT || 4000;
+//For Showing if Server is Up
+app.get("/", (req, res) => {
+	res.send("<h1>Socket Server up and Running...</h1>");
+});
+
+//setup socket  End Points
+io.on("connection", (socket) => {
+	console.log("Client Connected");
+
+	//Listener for joining room
+	socket.on("join", (projectId, entityId) => {
+		const room = projectId.toLowerCase().concat(entityId.toLowerCase());
+		socket.join(room);
+		console.log("Client is connected to Room", room);
+	});
+
+	//Listener to update Location of Entity
+	socket.on("update-location", (projectId, entityId, location) => {
+		const room = projectId.toLowerCase().concat(entityId.toLowerCase());
+		//emits entity-new-location event to room (projectId-entityId);
+		socket.broadcast.to(room).emit("entity-new-location", location);
+	});
+
+	//Listener for Leaving room
+	socket.on("leave", (projectId, entityId) => {
+		const room = projectId.toLowerCase().concat(entityId.toLowerCase());
+		socket.leave(room);
+		console.log("Client left room", room);
+	});
+
+	io.on("disconnect", () => {
+		console.log("Client Disconnected");
+	});
+});
+
+//----------------------------------------------------------
+//Start Server
+server.listen(port, () => {
+	console.log("Server Up and running at Port :" + port);
+});
